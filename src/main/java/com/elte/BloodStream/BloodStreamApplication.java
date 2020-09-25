a33d81152c055b513941e564f39e6fceffe34571
gergoterek <gergoterek@gmail.com>
2020-09-25 10:51:10 +0200

    initalizing with Spring Boot Initalizr

---
+++ src/main/java/com/elte/BloodStream/BloodStreamApplication.java
@@ -0,0 +1,13 @@
+package com.elte.BloodStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elte/BloodStream/BloodStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elte.BloodStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
